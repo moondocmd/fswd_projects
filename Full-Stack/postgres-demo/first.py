dcb4bb3a9af06557500a665b7421c763610bab11
moondocmd <moondocmd@gmail.com>
2020-08-01 14:54:45 -0700

    initializing

---
+++ Full-Stack/postgres-demo/first.py
@@ -0,0 +1,40 @@
+
+
+import psycopg2
+
+connection = psycopg2.connect(
+		host = "localhost",
+		database =  "Mikedb",
+		user = "postgres",
+		password ="pass"
+	)
+
+print("Connecting to database")
+
+cursor = connection.cursor()
+
+cursor.execute('DROP TABLE IF EXISTS table2;')
+
+cursor.execute('''
+  CREATE TABLE table2 (
+    id INTEGER PRIMARY KEY,
+    completed BOOLEAN NOT NULL DEFAULT False
+  );
+''')
+
+cursor.execute('INSERT INTO table2 (id, completed) VALUES (%s, %s);', (1, True))
+
+SQL = 'INSERT INTO table2 (id, completed) VALUES (%(id)s, %(completed)s);'
+
+data = {
+  'id': 2,
+  'completed': False
+}
+cursor.execute(SQL, data)
+
+connection.commit()
+
+print("Connected!\n")
+
+connection.close()
+cursor.close()
